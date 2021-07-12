5a5e565886ed6d075b8e5266a035dc7eec898b1f
admin-on-win10 <shashwatjaiswal14@gmail.com>
2021-07-12 14:37:16 +0530

    added more animations

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const hiddenNavbar = document.querySelector('.collapsed-nav');
 const page2img = document.querySelector('.page2-left img');
 const page4img = document.querySelectorAll('.page4 img');
 const underline = document.querySelectorAll('.underline');
+const homeImage = document.querySelector('.content-right img');
+const page3img = document.querySelectorAll('.page3-col-2 img');
 
 // initializations
 function init(element,eclass) {
@@ -17,6 +19,7 @@ function replace(element,rclass,aclass) {
 
 init(hiddenNavbar,'opacity');
 init(hiddenNavbar,'display-none');
+
 init(page2img,'scale');
 page4img.forEach(element => {
    init(element,'scale');
@@ -26,6 +29,26 @@ underline.forEach(element=>{
    init(element,'scaleX');
    init(element,'transition');
 });
+
+page3img.forEach(element => {
+   init(element,'fade-up');
+});
+
+// init(homeImage,'fade-right');
+
+// slide in animation onload
+
+// window.onload = function () {
+//    setTimeout(() => {
+//       replace(homeImage,'fade-right','fade-right-in');
+//    }, 0);
+// }
+
+// home image animation
+window.setTimeout(() => {
+   replace(homeImage,'fade-right','fade-right-in');
+}, 300);
+
 
 // toggle hidden navbar
 
@@ -45,6 +68,8 @@ function offset(element) {
 
 const array1 = [page2img,page4img[0],page4img[1],page4img[2]];
 const array2 = [...underline];
+const array3 = [page3img[0],page3img[1],page3img[2]];
+// wow spread not working for array3
 
 
 window.onscroll = function change() {
@@ -59,11 +84,20 @@ window.onscroll = function change() {
 
    // loop for underline
    array2.forEach(element => {
-      if (window.scrollY>offset(element)-500) {
+      if (window.scrollY>offset(element)-525) {
          replace(element,'scaleX','scaleX-in');
          array2.shift(); 
    }
    });   
+   
+   // loop for page3 images fade-up
+   array3.forEach(element => {
+      if (window.scrollY>offset(element)-550) {
+         replace(element,'fade-up','fade-up-in');
+         // array3.shift(); 
+   }
+   });   
+   
 
 }
 
